57a52f567deafd2375348d2259fd5441b20187c1
DiegoAlbarrracin <diegoalbarracin0@gmail.com>
2022-11-05 02:58:36 -0300

    Microservicio en Python completo

--- app.py
+++ app.py
@@ -25,9 +25,17 @@ class User(BaseModel):
     email:  str 
     procesadoPor:  str 
 
-@app.get("/")
+
+#Modelo para responder un token en formato Json
+class Json(BaseModel):
+    token = "tokenDePythonEnNodejs"
+
+json = Json()
+
+@app.get("/tokenPython")
 def read_root():
-    return users
+    
+    return (json.dict())
 
 
 @app.post("/msPython")
@@ -39,21 +47,26 @@ def get_post(user: User):
        user.idCliente = user.idCliente + 1
        user.procesadoPor = procesadoPorPy
        users.append(user.dict())
-       #print(users[2]);
        enviarCorreo()
+       #Vaciamos el array de objetos para que no se stackeen los resultados 
+	   #mas alla de lo necesario, empiece a devolver valores repetidos y los envie por mail
+       users.clear()
     
     
     #Guardo el json que me envia Node en el array users[]
-    return user;
+    print(users)
+    return users;
 
 
+
+#Funcion para enviar correo sendgrid en python
 def enviarCorreo():
 
     message = Mail(
     from_email='diegoalbarracin0@gmail.com',
     to_emails='diegosteamnew@hotmail.com',
     subject='TP Microservicios Node, Go y Python',
-    html_content='<strong style="color:green">' + str(users[0]) +'<strong>' + ',<br><br>' + '<strong style="color:blue">' + str(users[0]) +'<strong>' + ',<br><br>' + '<strong style="color:yellow">' + str(users[0]) +'<strong>' + ',<br><br>')
+    html_content='<strong style="color:green">' + str(users[0]) +'<strong>' + ',<br><br>' + '<strong style="color:blue">' + str(users[1]) +'<strong>' + ',<br><br>' + '<strong style="color:yellow">' + str(users[2]) +'<strong>' + ',<br><br>')
 
     try:
         sg =SendGridAPIClient(config('TOKEN'))
